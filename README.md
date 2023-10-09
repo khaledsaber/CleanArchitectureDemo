acd4e996825a1ac504c7659c0f2d5425ab4512cd
khaledsaber <khaledsaber6646@gmail.com>
2023-10-10 01:42:26 +0200

    add renderer implementer

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-startFrom: Lec 95
+startFrom: Lec 97
  
 
      
--- lib/presentation/common/state_renderer/state__renderer_impl.dart
+++ lib/presentation/common/state_renderer/state__renderer_impl.dart
@@ -1,21 +1,76 @@
+import 'package:clean_architecture_demo/app/constants.dart';
 import 'package:clean_architecture_demo/presentation/common/state_renderer/state_renderer.dart';
 import 'package:clean_architecture_demo/presentation/resources/strings_manager.dart';
+import 'package:flutter/material.dart';
 
-abstract class FlowState{
+abstract class FlowState {
   StateRendererType getStateRendererType();
+
   String getMessage();
 }
 
-class LoadingState extends FlowState{
+class LoadingState extends FlowState {
   StateRendererType stateRendererType;
   String? message;
 
-
-  LoadingState({required this.stateRendererType, this.message = AppStrings.loading});
-
-  @override
-  String getMessage()=> message!;
+  LoadingState(
+      {required this.stateRendererType, this.message = AppStrings.loading});
 
   @override
-  StateRendererType getStateRendererType() =>stateRendererType;
+  String getMessage() => message!;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+
+  ErrorState(this.stateRendererType, this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+class ContentState extends FlowState {
+  ContentState();
+
+  @override
+  String getMessage() => Constants.empty;
+
+  @override
+  StateRendererType getStateRendererType() => StateRendererType.CONTETNT_STATE;
+}
+
+class EmptyState extends FlowState {
+  String message;
+
+  EmptyState(this.message);
+
+  @override
+  String getMessage() => message;
+
+  @override
+  StateRendererType getStateRendererType() =>
+      StateRendererType.FULL_SCREEN_EMPTY_STATE;
+}
+
+
+
+extension FlowStateExtension on FlowState{
+  Widget getScreenWidget(BuildContext context,Widget contentScreen, Function retryActionFunction){
+
+     return switch(this.runtimeType){
+       LoadingState=>
+     ErrorState
+       EmptyState
+     ContentState
+     }
+
+
+  }
 }
